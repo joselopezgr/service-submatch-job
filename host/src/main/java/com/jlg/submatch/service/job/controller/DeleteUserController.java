b26511cdf0c834d6f4f581ead8b22ee626e569cc
Jose Lopez <jose.guerrero@tes.com>
2024-05-08 15:39:20 +0100

    Fixing some typo

--- host/src/main/java/com/jlg/submatch/service/job/controller/DeleteUserController.java
+++ host/src/main/java/com/jlg/submatch/service/job/controller/DeleteUserController.java
@@ -6,7 +6,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/job/{id}")
+@RequestMapping("/jobs/{id}")
 public class DeleteUserController {
     private final JobService jobService;
 
--- host/src/main/java/com/jlg/submatch/service/job/controller/UpdateJobController.java
+++ host/src/main/java/com/jlg/submatch/service/job/controller/UpdateJobController.java
@@ -8,7 +8,7 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/job/{id}")
+@RequestMapping("/jobs/{id}")
 public class UpdateJobController extends AbstractJobController {
 
     final JobService jobService;
